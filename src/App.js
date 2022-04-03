8ae3a5c9ae87d04d7bca583bb180962c6e008708
Julian Lindsay-Kaufman <jjjuliannnn456@gmail.com>
2022-04-02 20:09:00 -0400

    added home, about, and notfound routes with limited functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 
@@ -9,7 +12,12 @@ function App() {
         <Navbar />
 
         <main className='mx-auto px-3 pb-12'>
-          Content
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/about' element={<About />}></Route>
+            <Route path='/notfound' element={<NotFound />}></Route>
+            <Route path='/*' element={<NotFound />}></Route>
+          </Routes>
         </main>
 
         <Footer />

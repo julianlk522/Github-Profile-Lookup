cfd42960463a1b98758bc08714c4565541b9e1fa
Julian Lindsay-Kaufman <jjjuliannnn456@gmail.com>
2022-04-04 12:53:24 -0400

    user search component and functionality, clear search button

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,11 +1,10 @@
 import { createContext, useReducer } from "react";
-import UserResults from "../../components/users/UserResults";
 import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext()
 
-const GITHUB_URL = 'https://api.github.com/users'
-const GITHUB_TOKEN = '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'
+const GITHUB_URL = 'https://api.github.com'
+const GITHUB_TOKEN = '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'
 
 export const GithubProvider = ({children}) => {
     const initialState = {
@@ -15,31 +14,40 @@ export const GithubProvider = ({children}) => {
 
     const [state, dispatch] = useReducer(githubReducer, initialState)
     
-    // get initial users
-    const fetchUsers = async () => {
+    // get search results
+    const searchUsers = async (text) => {
         setLoading()
         
-        const response = await fetch(GITHUB_URL, {
+        const params = new URLSearchParams({
+            q: text
+        })
+        
+        const response = await fetch(
+            `${GITHUB_URL}/search/users?${params}`, {
             headers: {
                 Authorization: `token ${GITHUB_TOKEN}`
             },
         })
 
-        const data = await response.json()
+        const {items} = await response.json()
 
         dispatch({
             type: 'GET_USERS',
-            payload: data
+            payload: items
         })
     }
 
+    // clear users from state
+    const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
+    
     // set loading
     const setLoading = () => dispatch({type: 'SET_LOADING'})
     
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers,
+        searchUsers,
+        clearUsers
     }}>
         {children}
     </GithubContext.Provider>
